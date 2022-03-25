f9dd77ca8b8ea6f8d10573428fa05e7255183b0e
GitHub Action <action@github.com>
2022-03-25 18:22:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/karina5005/memoryanalysis/MyBundle.kt
+++ src/main/kotlin/com/github/karina5005/memoryanalysis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.karina5005.memoryanalysis
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/karina5005/memoryanalysis/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/karina5005/memoryanalysis/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.karina5005.memoryanalysis.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.karina5005.memoryanalysis.MyBundle
 
 class MyApplicationService {
 
